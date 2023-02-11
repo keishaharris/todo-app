9f94724eb5c4559c2df8f649f928d129cb8413ff
keishaharris <keishadharriss@gmail.com>
2023-02-11 11:24:43 -0500

    Added Form, Add task to local Storage feature

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-alert('its working');
+// Model (data)
+class task  {
+    constructor(title,description,priority,notes,date){
+    this.title = title,
+    this.description = description,
+    this.priority = priority,
+    this.notes = notes,
+    this.date = date
+    }
+}
+
+function handleSumbit(e){
+    e.preventDefault();
+
+    const title = document.getElementById('title').value;
+    const description = document.getElementById('description').value;
+    const priority = document.getElementById('priority').value;
+    const notes = document.getElementById('notes').value;
+    const dueDate = document.getElementById('date').value;
+
+
+    let newTask = new task(title, description, priority, notes, dueDate)
+    localStorage.setItem("newtask", JSON.stringify(newTask));
+
+return localStorage;
+}
+
+// View(Display Data/DOM manipulation)
+const content= document.getElementById('content');
+
+
+// Controller (mediator)
+const submitBtn = document.getElementById('task-form');
+submitBtn.addEventListener('submit', handleSumbit);
+
+
+
+
+
+
+
+
+
